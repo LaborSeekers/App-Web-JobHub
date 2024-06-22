a5e7d62aac871df01c3902c12d7733975af60426
Focas2003 <joacovelarde0@gmail.com>
2024-06-22 17:40:29 -0500

    feat:Curriculum , ingresar datos y entrevistas

---
+++ src/app/postulantes/curriculum/editar-curriculum/editar-curriculum.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-curriculum',
+  templateUrl: './editar-curriculum.component.html',
+  styleUrl: './editar-curriculum.component.css'
+})
+export class EditarCurriculumComponent {
+
+}
