933d7f393b6ab07fcfff691129ca01b6b47be1cd
KarimS21 <U201816862@upc.edu.pe>
2024-06-22 04:16:11 -0500

    Feat: Se implementó la interfaz de alertas de trabajo

--- src/app/postulantes/postulantes-routing.module.ts
+++ src/app/postulantes/postulantes-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { PostulantesComponent } from './postulantes.component';
 import { TablaEmpleoComponent } from './tabla-empleo/tabla-empleo.component';
 import { TablaEmpleoFavComponent } from './tabla-empleo-fav/tabla-empleo-fav.component';
+import { AlertasTrabajoComponent } from './alertas-trabajo/alertas-trabajo.component';
 
 const routes: Routes = [
   { path: '', component: PostulantesComponent,
@@ -10,6 +11,7 @@ const routes: Routes = [
       {path: '', component: TablaEmpleoComponent},
       {path: 'TablaOV', component: TablaEmpleoComponent},
       {path: 'TablaFav', component: TablaEmpleoFavComponent},
+      {path: 'AlertasTrabajo', component:AlertasTrabajoComponent},
       {path: 'appconfiguration', loadChildren: () => import('./appconfiguration/appconfiguration.module').then(m => m.AppconfigurationModule) },
       ]
    },
